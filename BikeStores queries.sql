8caa059a0241a65619c5bc62340a9814895c3e2d
EdwngJs <139380995+EdwngJs@users.noreply.github.com>
2023-12-23 18:45:40 -0400

    Add files via upload

---
+++ BikeStores queries.sql
@@ -0,0 +1,19 @@
+-- Joining tables
+
+SELECT ord.order_id, CONCAT(cus.first_name, ' ', cus.last_name) AS customer, cus.city, cus.state, ord.order_date, 
+SUM(ite.quantity) AS total_units, SUM(ite.quantity * ite.list_price) AS revenue,
+prod.product_name, cat.category_name, sto.store_name, CONCAT(sta.first_name, ' ', sta.last_name) AS sales_rep
+FROM sales.orders ord
+JOIN sales.customers cus
+ON ord.customer_id = cus.customer_id
+JOIN sales.order_items ite
+ON ord.order_id = ite.order_id
+JOIN production.products prod
+ON ite.product_id = prod.product_id
+JOIN production.categories cat
+ON prod.category_id = cat.category_id
+JOIN sales.stores sto
+ON ord.store_id = sto.store_id
+JOIN sales.staffs sta
+ON ord.staff_id = sta.staff_id
+GROUP BY ord.order_id, CONCAT(cus.first_name, ' ', cus.last_name), cus.city, cus.state, ord.order_date, prod.product_name, cat.category_name, sto.store_name, CONCAT(sta.first_name, ' ', sta.last_name);
